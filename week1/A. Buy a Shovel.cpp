b9f4b3a232c4932a6d734a5402e59ded4018fa98
Radwa15 <65210714+Radwa15@users.noreply.github.com>
2021-09-04 22:12:05 +0200

    Create A. Buy a Shovel.cpp

---
+++ week1/A. Buy a Shovel.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ int s , c , res=1 ;
+    cin>>s ;
+    cin >>c;
+    while(true)
+      {
+        res ++;
+        if ( (s*res)%10 == ch || (sh*res)%10 == 0 )
+       {
+        cout<< res ; 
+        return 0;
+        }
+        
+    }
+}
