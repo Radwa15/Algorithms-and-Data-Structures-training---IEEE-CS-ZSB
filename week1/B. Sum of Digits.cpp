1df0574d4f0b38d9cac0fd748e20385c17781ee1
Radwa15 <65210714+Radwa15@users.noreply.github.com>
2021-09-04 22:14:11 +0200

    Create B. Sum of Digits.cpp

---
+++ week1/B. Sum of Digits.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include<string>
+
+using namespace std;
+     
+int main() {
+     
+    string s1;                     
+    cin >> s1;                         
+     
+    int count = 0, x = 11;
+     
+    if (s1.length() < 2)
+         {
+    	cout << 0;                      
+    	return 0;
+          }
+     
+    while (x >= 10)
+    {
+    	x = 0;
+    	for (int i = 0;i < s1.length();i++)
+    	{
+    	       x += (s1[i] - '0');                
+    	}
+     
+    	s1 = to_string(x);                      
+     
+    	count++;                                     
+    }
+    cout << count;
+    return 0;
+ }
+    
